dedf1be1e95f7e77ae79d14b4d78a6e5e01bc5d6
ez0bappa <bappa.einzigtech@gmail.com>
2022-06-06 09:48:57 +0530

    [WIP] Remove chart js package

--- src/components/ExpenseBudget/ChartGraphs/Doughnut.vue
+++ src/components/ExpenseBudget/ChartGraphs/Doughnut.vue
@@ -1,10 +1,13 @@
 <template>
     <div class="doughnut-chart">
         <h4>Doughnut</h4>
+        <!-- <canvas id="planet-chart"></canvas> -->
     </div>
 </template>
 
 <script>
+    // import Chart from 'chart.js'
+
     export default {
         name: 'Doughnut'
     }
